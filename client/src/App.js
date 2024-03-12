58baf2cd2f82cddedef603406669761495f39ae7
Anant-02 <anantupadhyay02@gmail.com>
2024-03-12 15:41:18 +0530

    App.js Update With Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
@@ -6,9 +7,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path = "/" Component={Login}></Route>
-        </Switch>
+        <Routes>
+          <Route exact path = "/" element={<Login/>}></Route>
+          <Route exact path = "/register" element={<Register/>}></Route>
+          <Route exact path = "/reset" element={<Reset/>}></Route>
+          <Route exact path = "/dashboard" element={<Dashboard/>}></Route>
+        </Routes>
       </Router>
     </div>
   );
