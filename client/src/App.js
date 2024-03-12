ac1f59df56c8c9c7467d5f5d2435716bdd1f335c
Anant-02 <anantupadhyay02@gmail.com>
2024-03-12 14:53:21 +0530

    Login Page Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Login from './components/Login';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path = "/" Component={Login}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -59,3 +59,52 @@ const signInWithGoogle = async() => {
         alert(err.message);
     }
 }
+
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+}
+
+const registerWithEmailAndPassword = async (email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "users"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+}
+
+const sendPasswordReset = async (email) => {
+    try {
+        await sendPasswordResetEmail (auth, email);
+        alert("Password reset link sent!");
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+}
+
+const logout = () => {
+    signOut(auth);
+}
+
+export {
+    auth,
+    db,
+    signInWithGoogle,
+    logInWithEmailAndPassword,
+    registerWithEmailAndPassword,
+    sendPasswordReset,
+    logout
+};
